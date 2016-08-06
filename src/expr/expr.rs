efa848454596030c4a0666fc2ed40039e795d8ef
Alex Ozdemir <aozdemir@hmc.edu>
2016-08-06 16:49:57 -0400

    Non-Bonus completed

--- src/expr/expr.rs
+++ src/expr/expr.rs
@@ -4,6 +4,8 @@
 // The definition of `Expr`, a type that represents arithmetic expressions involving +,-,*,/, in
 // terms of those operations.
 
+use std::{cmp,mem};
+
 pub enum Expr {
     BinOp(Box<Expr>, BinOp, Box<Expr>),
     Literal(isize),
@@ -18,19 +20,38 @@ pub enum BinOp {
 
 impl Expr {
     pub fn evaluate(&self) -> isize {
-        unimplemented!()
+        match *self {
+            Expr::BinOp(ref left, BinOp::Plus, ref right) => left.evaluate() + right.evaluate(),
+            Expr::BinOp(ref left, BinOp::Minus, ref right) => left.evaluate() - right.evaluate(),
+            Expr::BinOp(ref left, BinOp::Times, ref right) => left.evaluate() * right.evaluate(),
+            Expr::BinOp(ref left, BinOp::Over, ref right) => left.evaluate() / right.evaluate(),
+            Expr::Literal(int) => int,
+        }
     }
 
     /// Computes the number of binary operations.
     /// For example, `1+4-5` has two operations.
     pub fn operation_count(&self) -> usize {
-        unimplemented!()
+        match *self {
+            Expr::BinOp(ref left, _, ref right) =>
+                left.operation_count() + 1 + right.operation_count(),
+            Expr::Literal(_) => 0,
+        }
     }
 
     /// The depth, defined as `max{ # of operations from root to leaf }`.
     /// `1` has depth 0, `1+3` has depth 1, and `1+4*3` has depth 2
     pub fn depth(&self) -> usize {
-        unimplemented!()
+        match *self {
+            Expr::BinOp(ref left, _, ref right) => 1 + cmp::max(left.depth(), right.depth()),
+            Expr::Literal(_) => 0,
+        }
     }
+
+    pub fn do_op(&mut self, operation: BinOp, operand: Expr) {
+        let this = mem::replace(self, Expr::Literal(0));
+        *self = Expr::BinOp(Box::new(this), operation, Box::new(operand));
+    }
+
 }
 
--- src/expr/parser.rs
+++ src/expr/parser.rs
@@ -3,56 +3,65 @@
 //
 // The parser for an `Expr` (currently just produces the value fo the `Expr`)
 
+use super::{Expr, BinOp};
+
 use nom::digit;
 
 use std::str;
 use std::str::FromStr;
 
-named!(parens<i64>, delimited!(
+named!(parens<Expr>, delimited!(
     char!('('),
     expr,
     char!(')')
   )
 );
 
-named!(i64_literal<i64>,
-  map_res!(
+named!(isize_literal<Expr>,
+  map!(
     map_res!(
-      digit,
-      str::from_utf8
+      map_res!(
+        digit,
+        str::from_utf8
+      ),
+      FromStr::from_str
     ),
-    FromStr::from_str
+    Expr::Literal
   )
 );
 
-named!(factor<i64>,
+named!(factor<Expr>,
   alt!(
-    i64_literal
+    isize_literal
   | parens
   )
 );
 
 // we define acc as mutable to update its value whenever a new term is found
-named!(term <i64>,
+named!(term <Expr>,
   chain!(
     mut acc: factor  ~
              many0!(
                alt!(
-                 tap!(mul: preceded!(tag!("*"), factor) => acc = acc * mul) |
-                 tap!(div: preceded!(tag!("/"), factor) => acc = acc / div)
+                 map!(preceded!(tag!("*"), factor),
+                      |mul| acc.do_op(BinOp::Times, mul)) |
+                 map!(preceded!(tag!("/"), factor),
+                      |div| acc.do_op(BinOp::Over, div))
                )
              ),
     || { return acc }
   )
 );
 
-named!(pub expr <i64>,
+named!(pub expr <Expr>,
   chain!(
     mut acc: term  ~
              many0!(
                alt!(
-                 tap!(add: preceded!(tag!("+"), term) => acc = acc + add) |
-                 tap!(sub: preceded!(tag!("-"), term) => acc = acc - sub)
+                 map!(preceded!(tag!("+"), term),
+                      |add| acc.do_op(BinOp::Plus, add)) |
+                 map!(preceded!(tag!("-"), term),
+                      |sub| acc.do_op(BinOp::Minus, sub))
                )
              ),
     || { return acc }
