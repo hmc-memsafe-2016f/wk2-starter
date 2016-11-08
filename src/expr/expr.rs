ccb8ef87ffc35f6cf1a93abde1758bbe7940bafb
Robert "Skipper" Gonzalez <robertgonzalez@WL-218-232.WPA.Claremont.Edu>
2016-11-08 14:38:53 -0800

    Still can't figure out parse

--- src/expr/expr.rs
+++ src/expr/expr.rs
@@ -4,9 +4,12 @@
 // The definition of `Expr`, a type that represents arithmetic expressions involving +,-,*,/, in
 // terms of those operations.
 
+use std::mem;
+use std::cmp;
+
 pub enum Expr {
     BinOp(Box<Expr>, BinOp, Box<Expr>),
-    Literal(isize),
+    Literal(i64),
 }
 
 pub enum BinOp {
@@ -17,20 +20,37 @@ pub enum BinOp {
 }
 
 impl Expr {
-    pub fn evaluate(&self) -> isize {
-        unimplemented!()
+    pub fn evaluate(&self) -> i64 {
+        let theVal = match self {
+            &Expr::Literal(i) => i,
+            &Expr::BinOp(ref a, ref b, ref c) => 
+                match b {
+                    &BinOp::Plus => a.evaluate() + c.evaluate(),
+                    &BinOp::Minus => a.evaluate() - c.evaluate(),
+                    &BinOp::Times => a.evaluate() * c.evaluate(),
+                    &BinOp::Over => a.evaluate() / c.evaluate()
+                }
+        };
+        //print!("{:?}", theVal);
+        theVal
     }
 
     /// Computes the number of binary operations.
     /// For example, `1+4-5` has two operations.
     pub fn operation_count(&self) -> usize {
-        unimplemented!()
+        match self { //??
+            &Expr::Literal(i) => 0,
+            &Expr::BinOp(ref a, ref b, ref c) => 1 + a.operation_count() + c.operation_count()
+        }
     }
 
     /// The depth, defined as `max{ # of operations from root to leaf }`.
     /// `1` has depth 0, `1+3` has depth 1, and `1+4*3` has depth 2
     pub fn depth(&self) -> usize {
-        unimplemented!()
+        match self { //??
+            &Expr::Literal(i) => 0,
+            &Expr::BinOp(ref a, ref b, ref c) => cmp::max(1 + a.operation_count(), 1 + c.operation_count())
+        }
     }
 }
 
