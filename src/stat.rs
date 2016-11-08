8f93a1d241326b6ae9b27178198e01bc3cd4bb51
Daniel <daniel.sonner@gmail.com>
2016-11-08 12:01:21 -0800

    added stat.rs file

---
+++ src/stat.rs
@@ -0,0 +1,27 @@
+// Daniel Sonner
+// Starter code for HMC's MemorySafe, week 2
+//
+// The Read Evaluate Print Loop
+
+#[macro_use]
+extern crate nom;
+
+mod expr;
+
+use nom::IResult;
+use std::io;
+
+fn main() {
+    let mut line = String::new();
+
+    while io::stdin().read_line(&mut line).map(|l| l > 0).unwrap_or(false) {
+
+        match expr::parse(line.as_str().trim().as_bytes()) {
+            IResult::Done(rest, ref res) if rest.len() == 0 => println!("{} {} {}"
+            	, res.evaluate(), res.operation_count(), res.depth()),
+            _ => println!("Error"),
+        }
+
+        line.clear();
+    }
+}
