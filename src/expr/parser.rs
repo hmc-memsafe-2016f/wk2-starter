7aea17626f92de8a1c8b3b604dac37dce1ed23ee
Zach Hauser <zehauser@gmail.com>
2016-11-07 19:04:14 -0800

    This would work were it not for that pesky left-recursion

--- src/expr/parser.rs
+++ src/expr/parser.rs
@@ -5,56 +5,63 @@
 
 use nom::digit;
 
+use expr::Expr;
+use expr::BinOp;
 use std::str;
 use std::str::FromStr;
 
-named!(parens<i64>, delimited!(
+named!(parens<Expr>, delimited!(
     char!('('),
     expr,
     char!(')')
   )
 );
 
-named!(i64_literal<i64>,
-  map_res!(
+named!(i64_literal<Expr>,
+  map!(
     map_res!(
-      digit,
-      str::from_utf8
+      map_res!(
+        digit,
+        str::from_utf8
+      ),
+      FromStr::from_str
     ),
-    FromStr::from_str
+    Expr::Literal
   )
 );
 
-named!(factor<i64>,
+named!(factor<Expr>,
   alt!(
     i64_literal
   | parens
   )
 );
 
-// we define acc as mutable to update its value whenever a new term is found
-named!(term <i64>,
-  chain!(
-    mut acc: factor  ~
-             many0!(
-               alt!(
-                 tap!(mul: preceded!(tag!("*"), factor) => acc = acc * mul) |
-                 tap!(div: preceded!(tag!("/"), factor) => acc = acc / div)
-               )
-             ),
-    || { return acc }
+
+named!(term<Expr>,
+  alt!(
+    factor 
+  | chain!(
+      t: term ~ tag!("*") ~ f: factor, 
+      || {Expr::BinOp(Box::new(t), BinOp::Times, Box::new(f))}
+    )
+  | chain!(
+      t: term ~ tag!("/") ~ f: factor, 
+      || {Expr::BinOp(Box::new(t), BinOp::Over, Box::new(f))}
+    )
   )
 );
 
-named!(pub expr <i64>,
-  chain!(
-    mut acc: term  ~
-             many0!(
-               alt!(
-                 tap!(add: preceded!(tag!("+"), term) => acc = acc + add) |
-                 tap!(sub: preceded!(tag!("-"), term) => acc = acc - sub)
-               )
-             ),
-    || { return acc }
+named!(pub expr<Expr>,
+  alt!(
+    term 
+  | chain!(
+      e: expr ~ tag!("+") ~ t: term, 
+      || {Expr::BinOp(Box::new(e), BinOp::Plus, Box::new(t))}
+    )
+  | chain!(
+      e: expr ~ tag!("-") ~ t: term, 
+      || {Expr::BinOp(Box::new(e), BinOp::Minus, Box::new(t))}
+    )
   )
 );
