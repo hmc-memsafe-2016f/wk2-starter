d248ef1858ced7b2e49c1b9626adfcf13ed0e5b0
Julien Chien <jchien17@cmc.edu>
2016-11-09 20:54:00 -0800

    parser ish

--- src/expr/parser.rs
+++ src/expr/parser.rs
@@ -1,4 +1,4 @@
-// Alex Ozdemir <aozdemir@hmc.edu> // <- Your name should replace this line!
+// Julien Chien <jchien@hmc.edu>
 // Starter code for HMC's MemorySafe, week 2
 //
 // The parser for an `Expr` (currently just produces the value fo the `Expr`)
@@ -8,24 +8,27 @@ use nom::digit;
 use std::str;
 use std::str::FromStr;
 
-named!(parens<i64>, delimited!(
+named!(parens<Expr>, delimited!(
     char!('('),
     expr,
     char!(')')
   )
 );
 
-named!(i64_literal<i64>,
-  map_res!(
+named!(i64_literal<Expr>,
+  map (
     map_res!(
-      digit,
-      str::from_utf8
+      map_res!(
+        digit,
+        str::from_utf8
+      ),
+      FromStr::from_str
     ),
-    FromStr::from_str
+    Expr::Literal
   )
 );
 
-named!(factor<i64>,
+named!(factor<Expr>,
   alt!(
     i64_literal
   | parens
@@ -33,7 +36,7 @@ named!(factor<i64>,
 );
 
 // we define acc as mutable to update its value whenever a new term is found
-named!(term <i64>,
+named!(term <Expr>,
   chain!(
     mut acc: factor  ~
              many0!(
