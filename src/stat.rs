b7f4a26d7444a84343474448678889ead91f566f
Michael Sheely <msheely@hmc.edu>
2016-11-12 15:02:50 -0800

    Added new binary to print statistics and updated cargo.toml accordingly

---
+++ src/stat.rs
@@ -0,0 +1,27 @@
+// Michael Sheely <msheely@hmc.edu>
+// Implements the statistics printing for HMC's MemorySafe, week 2
+//
+// The prints statistics for an expression withing a Read Evaluate Print Loop
+
+#[macro_use]
+extern crate nom;
+
+mod expr;
+
+use nom::IResult;
+use std::io;
+
+fn main() {
+    let mut line = String::new();
+
+    while io::stdin().read_line(&mut line).map(|l| l > 0).unwrap_or(false) {
+
+        match expr::parse(line.as_str().trim().as_bytes()) {
+            IResult::Done(rest, ref res) if rest.len() == 0 =>
+                println!("{} {} {}", res.evaluate(), res.depth(), res.operation_count()),
+            _ => println!("Error"),
+        }
+
+        line.clear();
+    }
+}
